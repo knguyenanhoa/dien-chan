2fd2ff561cac80a4e2dffbaff985e76fda9ced28
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2017-07-25 21:33:57 +1000

    Modified to use partials

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+from functools import partial
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+
 from sets import Sets
 
 
-class LoginScreen(GridLayout):
+class MainMenu(GridLayout):
+    sets = Sets()
+
     def __init__(self, **kwargs):
-        sets = Sets()
-        super(LoginScreen, self).__init__(**kwargs)
+        super(MainMenu, self).__init__(**kwargs)
         self.cols = 1
-        for key in sets.list_sets().keys():
-            self.add_widget(Button(text=key))
+        self.initialize()
+
+    def initialize(self):
+        for key in self.sets.list_sets().keys():
+            button = Button(text=key)
+            button.bind(on_press=partial(self.show_positions,key))
+            self.add_widget(button)
+
+    def show_positions(self, *args):
+        self.clear_widgets()
+        label = Label(text=args[0], height=200)
+        self.add_widget(label)
+        button = Button(text='Close', height=15)
+        button.bind(on_press=partial(self.show_main_menu))
+        self.add_widget(button)
+        
+    def show_main_menu(self, *args):
+        self.clear_widgets()
+        for key in self.sets.list_sets().keys():
+            button = Button(text=key)
+            button.bind(on_press=partial(self.show_positions,key))
+            self.add_widget(button)
+
 
 class DienChanApp(App):
     def build(self):
-        return LoginScreen()
+        return MainMenu()
 
-
+#MAIN
 DienChanApp().run()
+
