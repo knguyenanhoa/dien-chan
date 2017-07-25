427ab47d745792a3a832a7b2605778d2858ab829
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2017-07-25 21:58:10 +1000

    make kwargs work

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from functools import partial
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 
 from sets import Sets
 
@@ -19,12 +19,12 @@ class MainMenu(GridLayout):
     def initialize(self):
         for key in self.sets.list_sets().keys():
             button = Button(text=key)
-            button.bind(on_press=partial(self.show_positions,key))
+            button.bind(on_press=partial(self.show_positions,key=key))
             self.add_widget(button)
 
-    def show_positions(self, *args):
+    def show_positions(self, *args, **kwargs):
         self.clear_widgets()
-        label = Label(text=args[0], height=200)
+        label = Label(text=kwargs['key'])
         self.add_widget(label)
         button = Button(text='Close', height=15)
         button.bind(on_press=partial(self.show_main_menu))
@@ -34,7 +34,7 @@ class MainMenu(GridLayout):
         self.clear_widgets()
         for key in self.sets.list_sets().keys():
             button = Button(text=key)
-            button.bind(on_press=partial(self.show_positions,key))
+            button.bind(on_press=partial(self.show_positions,key=key))
             self.add_widget(button)
 
 
