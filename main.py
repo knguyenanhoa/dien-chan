a38cf52836f7555226563b85963af9e5ef40ba36
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2017-07-25 20:39:33 +1000

    Basic ui

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from sets import Sets
+
 
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
+        sets = Sets()
         super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        self.cols = 1
+        for key in sets.list_sets().keys():
+            self.add_widget(Button(text=key))
 
 class DienChanApp(App):
     def build(self):
---
+++ sets.py
@@ -0,0 +1,11 @@
+class Sets():
+    def __init__(self):
+        pass
+
+    def list_sets(self):
+        return {
+            "Bo ho": [1,2,3,4,5],
+            "Bo bo am huyet": [12,142,155]
+        }
+        pass
+
