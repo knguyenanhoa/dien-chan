c2b62bfd5552a75ea82721d1179b5bac70181716
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2017-07-25 23:13:52 +1000

    Implement scatter for transformation and resizing

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.scatter import Scatter
+from kivy.graphics.transformation import Matrix
 
 from sets import Sets
 
@@ -23,21 +25,31 @@ class MainMenu(GridLayout):
 
     def show_positions(self, *args, **kwargs):
         self.clear_widgets()
-        #label = Label(text=kwargs['key'])
-        #self.add_widget(label)
-        image = Image(source="./images/do_hinh_dien_chan_2.png")
-        self.add_widget(image)
 
-        menu = GridLayout(cols=2, size_hint_y=None, height=100)
-        self.add_widget(menu)
-        button = Button(text='Close')
+        map = GridLayout(cols=1, size_hint_y=.9)
+        self.add_widget(map)
+        context_menu = GridLayout(cols=2, size_hint_y=.1)
+        self.add_widget(context_menu)
+
+        #map
+        scatter = Scatter(auto_bring_to_front=False)
+        scatter.apply_transform(Matrix().scale(6,6,1))
+        map.add_widget(scatter)
+        image = Image(source="./images/do_hinh_dien_chan_2.png")
+        scatter.add_widget(image)
+
+        #context_menu
+        button = Button(text='Back', size_hint_x=.3)
         button.bind(on_press=partial(self.show_main_menu))
-        menu.add_widget(button)
-        button = Button(text='Close')
-        button.bind(on_press=partial(self.show_main_menu))
-        menu.add_widget(button)
+        context_menu.add_widget(button)
+
+        steps = ""
+        for point in self.sets.list_sets()[kwargs['key']]:
+            steps += (" => %s" % point)
+        label = Label(text=steps, size_hint_x=.7)
+        context_menu.add_widget(label)
         
-    def show_main_menu(self, *args):
+    def show_main_menu(self, *args, **kwargs):
         self.clear_widgets()
         for key in self.sets.list_sets().keys():
             button = Button(text=key)
