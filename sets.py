ee7697aa6dcaef29d44cb31bffd405f37b3628ac
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2017-07-26 16:06:11 +1000

    Split main_menu into different file. Add def to sets

--- sets.py
+++ sets.py
@@ -7,12 +7,27 @@ class Sets():
         func = getattr(self, func_name) 
         return func()
 
+    def list_A(self):
+        pass
+    
+    def list_B(self):
+        pass
+
     def list_C(self):
+        pass
+    
+    def list_D(self):
+        pass
+
+    def list_E(self):
+        pass
+
+    def list_G(self):
         pass
 
     def list_H(self):
         return {
             "Ho ngua co": ['61L','61R','74L','74R','64L','64R','14L','14R'],
-            "Test": ['61R']
+            "Test": ['61R'],
         }
 
