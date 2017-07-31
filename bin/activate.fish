bb2d17c88b03e8856d15961bb0e28ef62fba2ca3
Klinkesorn Nguyen An Hoa <k.nguyen.an.hoa@gmail.com>
2017-08-01 01:07:43 +1000

    Add pyinstaller and packaging for osx

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Volumes/untitled/dien_chan"
+set -gx VIRTUAL_ENV "/Users/knah/Desktop/untitled2_mount/dien_chan"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
